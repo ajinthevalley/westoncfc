c660343707edc654832c67b4153abf3bbb01893b
ajinthevalley <ajkane123@gmail.com>
2017-07-28 20:30:07 -0300

    navbar.php

---
+++ navbar.php
@@ -0,0 +1,49 @@
+
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+    <div class="row">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+<a class="navbar-brand text-center" href="#">Weston Christian Fellowship</a></div>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="about.html">About</a>
+      </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Beliefs</a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="purpose.html">Purpose</a>
+          <a class="dropdown-item" href="covenant.html">Church Covenant</a>
+          <a class="dropdown-item" href="faith.html">Statement of Faith</a>
+        </div>
+    </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contact</a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="contact.html">Email and Phone</a>
+          <a class="dropdown-item" href="directions.html">Directions</a>
+        </div>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ministries</a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="schedule.html">Weekly Schedule</a>
+          <a class="dropdown-item" href="#">Awana</a>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="sermons.html">Sermons</a>
+      </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://westoncfc.wordpress.com/" target="_blank">Pastors Blog</a>
+      </li>
+    </ul>
+    </ul>
+    
+  </div>
+</nav>
+
